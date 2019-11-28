00d7593797f85b2a3fb8abf96d4c8fa10c146bf4
ChiefBahooti <m.lubberts02@gmail.com>
2019-11-29 00:57:03 +0100

    Pushing some starting code, environment seems to be working.

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+extern crate hyper;
+extern crate pretty_env_logger;
+
+use hyper::{Body, Request, Response, Server};
+use hyper::service::service_fn_ok;
+use hyper::rt::{self, Future};
+
+fn main() {
+
+    pretty_env_logger::init();
+    let addr = ([127,0,0,1], 3000).into();
+
+    let server = Server::bind(&addr)
+        .serve(|| {
+            // Onderzoeken
+            service_fn_ok(move |_: Request<Body>| {
+                Response::new(Body::from("Hello World!"))
+            })
+        })
+        .map_err(|e| eprintln!("server error: {}", e));
+
+        println!("Listening on http://{}", addr);
+
+        rt::run(server);
+}
+
